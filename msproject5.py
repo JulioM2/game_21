6be0fe5c79b29d88ccd2b695142862724809380e
julio <juliomaia27@gmail.com>
2023-03-17 11:52:33 -0300

    Add comment about what show_cards function does and removed default argument from the third argument of the function, which is a list and was set as a empty string as default.

--- msproject5.py
+++ msproject5.py
@@ -4,7 +4,8 @@ suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 value = {'Ace': [1, 11], 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10}
 
 
-def show_cards(list_cards, name, dealer_list_cards = ''):
+# Show cards and its value from player and dealer in the screen
+def show_cards(list_cards, name, dealer_list_cards):
     if name == 'player':
         for card in dealer_list_cards:
             if type(card.value) == type([]):
